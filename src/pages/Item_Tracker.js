2dd03698bf11d7ea9ea10d6a24559c9c1304bf0c
Anusan <kailaianu21@gmail.com>
2023-04-06 18:35:21 +0530

    Designed Item Tracker

--- src/pages/Item_Tracker.js
+++ src/pages/Item_Tracker.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import Container from 'react-bootstrap/esm/Container';
+import '../Item_Tracker.css';
+
+
 
 export default function Item_Tracker() {
   return (
-    <div>
-      <h1>Item_Tracker</h1>
-    </div>
+    <Container className='Contain'>
+    <div className='title'>Item Tracker</div>
+    <form action="#">
+    <table>
+  <tr>
+    <th>Ref.No</th>
+    <th>Name</th>
+    <th>Mobile No</th>
+    <th>in Location</th>
+    <th>Out Location</th>
+    <th>Created Date Time</th>
+    <th>Full details</th>
+
+  </tr>
+  <tr>
+    <td>3423455</td>
+    <td>Niamal Suriyanayake</td>
+    <td>0718240835</td>
+    <td>Colombo</td>
+    <td>Kandy</td>
+    <td>2023-02-22 16:30:34</td>
+    <td><button class="button">View</button></td>
+  </tr>
+  <tr>
+  <td>3423456</td>
+    <td>Mahesh</td>
+    <td>0718240835</td>
+    <td>Colombo</td>
+    <td>Kandy</td>
+    <td>2023-02-22 16:30:34</td>
+    <td><button class="button">View</button></td>
+  </tr>
+  <tr>
+    <td>3423457</td>
+    <td>Theekshana</td>
+    <td>0718240835</td>
+    <td>Colombo</td>
+    <td>Kandy</td>
+    <td>2023-02-22 16:30:34</td>
+    <td><button class="button">View</button></td>
+  </tr>
+
+  
+</table>
+        </form>
+    </Container>
   )
 }
