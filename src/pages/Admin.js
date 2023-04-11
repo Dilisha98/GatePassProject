7cc953dc135d5ce7c8fc19852e11748fd3b61e2f
Dilisha <dilishamihiruth@gmail.com>
2023-04-11 23:01:38 +0530

    Add Ramalka's AdminCode

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,9 +1,64 @@
 import React from 'react'
+import Container from 'react-bootstrap/esm/Container';
+import './Admin.css';
+import Button from 'react-bootstrap/Button';
+
 
 export default function Admin() {
   return (
-    <div>
-      <h1>Admin</h1>
-    </div>
+      <>
+      <Container className='Contain'>
+      <form action="#">
+                <div class="user-details">
+                    <div class="input-div">
+                        <div>
+                            <span class="details">Location</span>
+                            <input type="text" name="" class="input" placeholder="Enter Location Here"/> 
+                        </div>
+                    </div>
+                    <div class="input-div">
+                        <div>
+                            <span class="details">CSV file Upload </span>
+                            <input type="text" name="" class="input" placeholder="Upload Csv Here" />
+
+                       </div>     
+
+                    </div>  
+
+                 </div>
+
+            </form>
+            <Container className='btnContain'>
+                <button className='btnsubmit'>Submit</button>
+                </Container>
+               <table>
+  <tr>
+    <th>#</th>
+    <th>Location</th> 
+    <th>Action</th>
+  </tr>
+  <tr>
+    <td>01</td>
+    <td>Colombo</td>
+    <td>
+    <Button variant="success">Edit</Button>{' '}
+    <Button variant="danger">Delete</Button>{' '}
+    </td>
+
+  </tr>
+  <tr>
+    <td>02</td>
+    <td>Kurunegala</td>
+    <td>
+    <Button variant="success">Edit</Button>{' '}
+    <Button variant="danger">Delete</Button>{' '}
+    </td>
+    </tr>
+
+</table>
+        </Container>
+
+      </>   
+
   )
 }
